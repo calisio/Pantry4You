3938ec257ce9c5c224f6f1821dced1569d7c94b8
bbhambr <bbhambri@wustl.edu>
2023-03-21 14:56:05 -0500

    done with save favorite
    saveFavRecipes

--- Pages/Account/Account.js
+++ Pages/Account/Account.js
@@ -1,22 +1,104 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, {useState} from 'react';
-
+import { StyleSheet, Text, View, Button, FlatList, Image, Linking, SafeAreaView, StatusBar } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../../firebase';
 
 const Account = ({navigation, route}) => {
-    const handleLogout = route.params.handleLogout;
-    const uid = route.params.uid;
+  const handleLogout = route.params.handleLogout;
+  const uid = route.params.uid;
+  const [favorites, setFavorites] = useState([]);
 
-    const handleSubmit = () => {
-        console.log(handleLogout)
-        handleLogout();
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      try {
+        const favoritesRef = collection(db, `users/${uid}/favorites`);
+        const favoritesSnapshot = await getDocs(favoritesRef);
+        const favoritesList = [];
+        favoritesSnapshot.forEach((doc) => {
+          favoritesList.push(doc.data());
+        });
+        setFavorites(favoritesList);
+      } catch (error) {
+        console.log(error);
+      }
     };
 
-    return (
-        <View>
-            <Button title="Log Out" onPress={() => handleSubmit()} />
-            <Text> Hello, {uid} </Text>
-        </View>
-    );
+    fetchFavorites();
+  }, []);
+
+  const RecipeView = ({item}) => (
+    <View style={styles.recipeContainer}>
+      <Text style={styles.recipeTitle}>{item.title}</Text>
+      <Image
+        source={{ uri: item.imgUrl }}
+        style={styles.recipeImage}
+      />
+      <Text style={styles.recipeInstructions}
+        onPress={() => Linking.openURL(item.recipeUrl)}>
+        Instructions
+      </Text>
+    </View>
+  );
+
+  const handleSubmit = () => {
+    console.log(handleLogout)
+    handleLogout();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Log Out" onPress={() => handleSubmit()} />
+      <Text style={styles.header}> Hello, {uid} </Text>
+      <SafeAreaView style={styles.listContainer}>
+        <FlatList
+          data={favorites}
+          renderItem={RecipeView}
+          keyExtractor={(item) => item.recipeId}
+          ListHeaderComponent={() => <View style={{ height: 10 }} />}
+          ListFooterComponent={() => <View style={{ height: 10 }} />}
+          ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+        />
+      </SafeAreaView>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  listContainer: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  recipeContainer: {
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginHorizontal: 16,
+  },
+  recipeTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  recipeImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  recipeInstructions: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
+});
 
 export {Account};
--- Pages/Home/Home.js
+++ Pages/Home/Home.js
@@ -1,6 +1,10 @@
 import {Text, FlatList, SafeAreaView, View, Image, Linking, StyleSheet, StatusBar,} from 'react-native';
 import GetRecipesIds from './GetRecipesIds';
 import React, {useState, useEffect} from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { addDoc, deleteDoc, doc, getDocs, query, where, collection } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { Alert } from 'react-native';
 
 const Home = ({navigation, route}) => {
   console.log("HOME RENDERED");
@@ -8,33 +12,78 @@ const Home = ({navigation, route}) => {
   const [recipeList, setRecipeList] = useState([]);
   const [isRefreshing, setIsRefreshing] = useState(false);
 
-  //function used to get recipes
-  async function fetchRecipes() {
-    const recipesIds = await GetRecipesIds(uid);
-    let recipeListTemp = []
-    for (let i = 0; i<recipesIds.length; i++){
-      try{
-        const recipe = await fetch('https://api.spoonacular.com/recipes/'+recipesIds[i]+'/information?includeNutrition=false', {
+
+  const handleFavorite = async (recipeId) => {
+    try {
+      // Ensure recipeId is a string
+      const recipeIdString = String(recipeId);
+  
+      // Check if the "favorites" subcollection exists
+      const favoritesRef = collection(db, `users/${uid}/favorites`);
+      const favoritesSnapshot = await getDocs(favoritesRef);
+      const recipeExists = favoritesSnapshot.docs.some(doc => String(doc.data().recipeId) === recipeIdString);
+  
+      // If the recipe is not already in the "favorites" subcollection, add it
+      if (!recipeExists) {
+        // Get the recipe details
+        const recipe = await fetch('https://api.spoonacular.com/recipes/' + recipeIdString + '/information?includeNutrition=false', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
-            'x-api-key': '2b747b3902c148758955f81fcac5bb4f'
-          }
-          });
-          const data = await recipe.json();
-          const recipeObj = {
-            title: data['title'],
-            imgUrl: data['image'],
-            recipeUrl: data['sourceUrl'],
-            recipeId: data['id']
-          };
-          recipeListTemp.push(recipeObj);
+            'x-api-key': '2b747b3902c148758955f81fcac5bb4f',
+          },
+        });
+        const data = await recipe.json();
+        const recipeObj = {
+          title: data['title'],
+          imgUrl: data['image'],
+          recipeUrl: data['sourceUrl'],
+          recipeId: data['id'],
+        };
+  
+        // Add the recipe to the "favorites" subcollection
+        await addDoc(favoritesRef, recipeObj);
+        console.log('Recipe added to favorites');
+      } else {
+        console.log('Recipe already in favorites');
+        Alert.alert('Error', 'This recipe is already in your favorites.');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
+  //function used to get recipes
+  async function fetchRecipes() {
+    const recipesIds = await GetRecipesIds(uid);
+    let recipeListTemp = [];
+    for (let i = 0; i < recipesIds.length; i++) {
+      try {
+        // Ensure recipeId is a string
+        const recipeIdString = String(recipesIds[i]);
+        const recipe = await fetch('https://api.spoonacular.com/recipes/' + recipeIdString + '/information?includeNutrition=false', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': '2b747b3902c148758955f81fcac5bb4f',
+          },
+        });
+        const data = await recipe.json();
+        const recipeObj = {
+          title: data['title'],
+          imgUrl: data['image'],
+          recipeUrl: data['sourceUrl'],
+          recipeId: data['id'],
+        };
+        recipeListTemp.push(recipeObj);
       } catch (error) {
         console.log(error);
       }
     }
     setRecipeList(recipeListTemp);
   }
+  
 
   //on load, get recipes
   useEffect(() => {
@@ -50,16 +99,24 @@ const Home = ({navigation, route}) => {
   };
 
   const RecipeView = ({item}) => (
-    <View>
-      <Text>{item.title}</Text>
+    <View style={styles.recipeContainer}>
+      <Text style={styles.recipeTitle}>{item.title}</Text>
       <Image
         source={{ uri: item.imgUrl }}
-        style={{ width: 200, height: 200 }}
+        style={styles.recipeImage}
       />
-      <Text style={{color: 'blue'}}
+      <Text style={styles.recipeInstructions}
         onPress={() => Linking.openURL(item.recipeUrl)}>
         Instructions
       </Text>
+      <View style={styles.favoriteButton}>
+        <MaterialCommunityIcons
+          name="heart-outline"
+          color="#f44336"
+          size={24}
+          onPress={() => handleFavorite(item.recipeId)}
+        />
+      </View>
     </View>
   );
 
@@ -72,7 +129,7 @@ const Home = ({navigation, route}) => {
         keyExtractor={(item) => item.recipeId}
         ListHeaderComponent={() => <View style={{ height: 10 }} />}
         ListFooterComponent={() => <View style={{ height: 10 }} />}
-        ItemSeparatorComponent={() => <View style={{ height: 50 }} />}
+        ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
         onRefresh={handleRefresh}
         refreshing={isRefreshing}
       />
@@ -85,13 +142,31 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
   },
-  item: {
+  recipeContainer: {
     padding: 20,
-    marginVertical: 8,
+    backgroundColor: '#fff',
+    borderRadius: 10,
     marginHorizontal: 16,
   },
-  title: {
-    fontSize: 32,
+  recipeTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  recipeImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  recipeInstructions: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
+  favoriteButton: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
   },
 });
 
