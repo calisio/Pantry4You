b43346c77041c1bc643aafd8bb4f9f1678cf93ca
jberthoud7 <jamesberthoud@gmail.com>
2023-04-04 11:45:29 -0500

    added isInRadius util

---
+++ utils/isInRadius.js
@@ -0,0 +1,11 @@
+export async function isInRadius(lat1, long1, lat2, long2){
+    var p = 0.017453292519943295;    // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+            c(lat1 * p) * c(lat2 * p) * 
+            (1 - c((long2 - long1) * p))/2;
+
+    var distance = 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+
+    return distance < 5; //5 hardcoded as radius
+}
